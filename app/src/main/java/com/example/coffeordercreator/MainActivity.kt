067201d375835557064577150a5c3bb0ec565e3b
leon <leonstefanski@proton.me>
2025-03-13 16:16:02 +0100

    final commit

--- app/src/main/java/com/example/coffeordercreator/MainActivity.kt
+++ app/src/main/java/com/example/coffeordercreator/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.coffeordercreator
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +32,16 @@ class MainActivity : AppCompatActivity() {
         val imageViewCoffee = findViewById<ImageView>(R.id.image_view_coffee)
         val seekBarQuantity = findViewById<SeekBar>(R.id.seek_bar_quantity)
         val textViewQuantity = findViewById<TextView>(R.id.text_view_quantity)
+        val buttonOrder = findViewById<Button>(R.id.button_order)
+
+        seekBarQuantity.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                textViewQuantity.text = progress.toString()
+            }
+        })
+
 
         radioGroupCoffee.setOnCheckedChangeListener { _, checkedId ->
             when (checkedId) {
@@ -42,6 +58,23 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        buttonOrder.setOnClickListener {
+            val selectedRadioButton =
+                findViewById<RadioButton>(radioGroupCoffee.checkedRadioButtonId)
+            val checkBoxSugar = findViewById<CheckBox>(R.id.check_box_sugar)
+            val checkBoxMilk = findViewById<CheckBox>(R.id.check_box_milk)
+            val text = "Zamówienie złożone!\n" +
+                    "Rodzaj kawy: ${selectedRadioButton.text.toString()}\n" +
+                    "Dodatki: mleko - ${checkBoxMilk.isChecked.toString()}, " +
+                    "cukier - ${checkBoxSugar.isChecked.toString()}\n" +
+                    "Ilość: ${seekBarQuantity.progress.toString()}"
+
+
+            Log.d("MainActivity", text)
+        }
+
+
     }
 
 
